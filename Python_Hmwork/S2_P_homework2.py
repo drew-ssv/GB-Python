ce32c5101c69af7de76de6fde0ee7d90923ef55a
drew-ssv <drew.semenov@gmail.com>
2022-12-14 23:43:07 +0300

    start_work_S2_P_homework2

---
+++ Python_Hmwork/S2_P_homework2.py
@@ -0,0 +1,22 @@
+# 1.(https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=3&id_topic=33&id_problem=190)
+
+
+# 2.(https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=3&id_topic=33&id_problem=197)
+
+
+# 3.(https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=3&id_topic=34&id_problem=205)
+
+print ('Task 3')
+# act = int(input('Start 1/0: '))
+# if act != 1:
+    
+x = int(input())
+counter = 0
+while counter < 1:
+    for i in range(2, x + 1):
+        if x % i == 0:
+            counter =+1
+            print(i)
+        #break
+
+# 4.(https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=5&id_topic=114&id_problem=699)
